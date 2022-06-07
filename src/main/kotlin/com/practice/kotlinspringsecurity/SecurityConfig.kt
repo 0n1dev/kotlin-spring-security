5a20d4c4a5dc69d7422e3e3386346f4b5883ec86
0n1dev <all_mind@naver.com>
2022-06-07 20:38:37 +0900

    Sec03_Form Login 인증

--- src/main/kotlin/com/practice/kotlinspringsecurity/SecurityConfig.kt
+++ src/main/kotlin/com/practice/kotlinspringsecurity/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 
 @Configuration
 @EnableWebSecurity
@@ -13,13 +14,26 @@ class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests {
-                it.anyRequest()
-                    .authenticated()
-            }
-            .httpBasic(Customizer.withDefaults())
+        http.authorizeHttpRequests()
+            .anyRequest()
+            .authenticated()
 
         http.formLogin()
+//            .loginPage("/loginPage")
+            .defaultSuccessUrl("/")
+            .failureUrl("/login")
+            .usernameParameter("userId")
+            .passwordParameter("passwd")
+            .loginProcessingUrl("/login_proc")
+            .successHandler { request, response, authentication ->
+                println(authentication.name)
+                response.sendRedirect("/")
+            }
+            .failureHandler { request, response, exception ->
+                println(exception.message)
+                response.sendRedirect("/login")
+            }
+            .permitAll()
 
         return http.build()
     }
